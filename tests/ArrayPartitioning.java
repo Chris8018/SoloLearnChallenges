47a49c9c91d58bfc60b267f7d245ee60bc1473fe
Chris <Chris8018>
2019-07-11 16:43:21 +0700

    Update

---
+++ tests/ArrayPartitioning.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ArrayPartitioning {
+
+    @BeforeAll
+    public void setUp() {
+        // Code
+    }
+
+    @Test
+    public void testInitData() {
+        //
+    }
+}
+
